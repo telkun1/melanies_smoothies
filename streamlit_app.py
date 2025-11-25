0e86ca628a86ee3460a2570873bdb52c0939e969
telkun1 <173297112+telkun1@users.noreply.github.com>
2025-11-25 16:46:07 +0000

    added search_on column

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,10 +25,11 @@ cnx = st.connection("snowflake")
 session = cnx.session()
 
 #session = get_active_session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
 
 #my_dataframe = session.table("smoothies.public.fruit_options")
 #st.dataframe(data=my_dataframe, use_container_width=True)
+#st.stop()
 
 ingredients_list = st.multiselect('Choose up to 5 ingredients:', my_dataframe, max_selections=5)
 
